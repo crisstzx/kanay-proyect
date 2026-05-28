f8b4465ce423e376f878cee068407989c6494907
crisstzx <113649129+crisstzx@users.noreply.github.com>
2026-05-28 17:24:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 1. Definimos las estructuras de datos que recibirá la API
 class ItemPedido(BaseModel):
